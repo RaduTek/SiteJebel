71057b86c1bf4b065348c1b1542f64386358483a
RaduTek <radutek13@gmail.com>
2024-05-12 18:07:44 +0300

    Include theme in layout

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,14 +1,15 @@
 import { CssBaseline, Toolbar } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import Navigation from "../components/Navigation";
+import Theme from "./Theme";
 
 export default function Main() {
     return (
-        <>
+        <Theme>
             <Navigation />
             <CssBaseline />
             <Toolbar />
             <Outlet />
-        </>
+        </Theme>
     );
 }
