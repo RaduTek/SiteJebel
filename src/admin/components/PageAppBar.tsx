5b7547e8ec6a4283d80adade5286a33ce694c8bb
RaduTek <radutek13@gmail.com>
2024-05-12 18:08:46 +0300

    Admin page appbar

---
+++ src/admin/components/PageAppBar.tsx
@@ -0,0 +1,45 @@
+import {
+    AppBar,
+    IconButton,
+    Toolbar,
+    Typography,
+    useMediaQuery,
+} from "@mui/material";
+import { useAtom } from "jotai";
+import { adminDrawerOpen } from "../atoms";
+import { Menu } from "@mui/icons-material";
+
+export default function PageAppBar({
+    title,
+    children,
+}: {
+    title: string;
+    children?: React.ReactNode;
+}) {
+    const isLargeScreen = useMediaQuery("(min-width: 900px)");
+    const [drawerOpen, setDrawerOpen] = useAtom(adminDrawerOpen);
+    const toggleDrawer = (newOpen: boolean) => () => {
+        setDrawerOpen(newOpen);
+    };
+
+    return (
+        <>
+            <AppBar sx={{ position: "sticky", width: "100%" }}>
+                <Toolbar>
+                    {!isLargeScreen && !drawerOpen && (
+                        <IconButton onClick={toggleDrawer(true)}>
+                            <Menu />
+                        </IconButton>
+                    )}
+                    <Typography
+                        variant="h4"
+                        sx={{ flex: 1, paddingLeft: "6px" }}
+                    >
+                        {title}
+                    </Typography>
+                    {children}
+                </Toolbar>
+            </AppBar>
+        </>
+    );
+}
