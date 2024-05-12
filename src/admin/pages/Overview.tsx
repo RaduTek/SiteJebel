7e25bed95cf4140fbffe26556d62816e834e5956
RaduTek <radutek13@gmail.com>
2024-05-12 18:09:02 +0300

    Admin overview page

---
+++ src/admin/pages/Overview.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+import PageAppBar from "../components/PageAppBar";
+
+export default function Overview() {
+    return (
+        <>
+            <PageAppBar title="Overview" />
+            <Typography variant="h1">Page test content</Typography>
+        </>
+    );
+}
