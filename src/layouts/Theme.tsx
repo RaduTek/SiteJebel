f92d3dcbce6b0429dc1583320a61307c76b0547f
RaduTek <radutek13@gmail.com>
2024-05-12 18:07:30 +0300

    Update theme

--- src/layouts/Theme.tsx
+++ src/layouts/Theme.tsx
@@ -26,6 +26,7 @@ const theme = createTheme({
             styleOverrides: {
                 colorPrimary: {
                     backgroundColor: "white",
+                    color: "black",
                 },
             },
         },
