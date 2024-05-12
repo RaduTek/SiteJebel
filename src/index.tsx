41de27d3cbb97c554799ef1c285d832abc0d48b1
RaduTek <radutek13@gmail.com>
2024-05-12 12:17:49 +0300

    Initial routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Main from "./layouts/Main";
+import Landing from "./pages/Landing";
+import Theme from "./layouts/Theme";
+
+const router = createBrowserRouter([
+    {
+        path: "/admin",
+        element: <div>Admin dashboard</div>,
+    },
+    {
+        path: "/",
+        element: <Main />,
+        children: [
+            {
+                path: "/",
+                element: <Landing />,
+            },
+            {
+                path: "/about",
+                element: <div>About page</div>,
+            },
+            {
+                path: "/events",
+                element: <div>Event list</div>,
+            },
+            {
+                path: "/courses",
+                element: <div>Course list</div>,
+            },
+        ],
+    },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Theme>
+            <RouterProvider router={router} />
+        </Theme>
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
