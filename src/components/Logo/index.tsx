d187605208cbdb5bfdbbf1271bc09d5fab362395
RaduTek <radutek13@gmail.com>
2024-05-12 12:16:32 +0300

    Logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,50 @@
+import { useMediaQuery, useTheme } from "@mui/material";
+import { ReactComponent as Banner } from "./banner.svg";
+import { ReactComponent as HeaderSmall } from "./header.svg";
+import { ReactComponent as HeaderLarge } from "./header_large.svg";
+import { ReactComponent as Footer } from "./footer.svg";
+
+export default function Logo({
+    variant,
+}: {
+    variant: "header" | "header_large" | "header_auto" | "footer" | "banner";
+}) {
+    const theme = useTheme();
+    const isLargeScreen = useMediaQuery("(min-width: 600px)");
+
+    const headerLogoStyle = {
+        fill: theme.palette.primary.main,
+        height: "60px",
+    };
+    const footerLogoStyle = {
+        fill: "#555",
+        height: "100px",
+    };
+    const bannerLogoStyle = {
+        fill: "white",
+        height: "200px",
+    };
+
+    const renderLogo = () => {
+        switch (variant) {
+            case "header":
+                return <HeaderSmall style={headerLogoStyle} />;
+            case "header_large":
+                return <HeaderLarge style={headerLogoStyle} />;
+            case "header_auto":
+                return isLargeScreen ? (
+                    <HeaderLarge style={headerLogoStyle} />
+                ) : (
+                    <HeaderSmall style={headerLogoStyle} />
+                );
+            case "footer":
+                return <Footer style={footerLogoStyle} />;
+            case "banner":
+                return <Banner style={bannerLogoStyle} />;
+            default:
+                return <></>;
+        }
+    };
+
+    return renderLogo();
+}
