eb4b0558996e08958fc7402bd325d241ca063f4c
RaduTek <radutek13@gmail.com>
2024-05-13 10:43:12 +0300

    Create events page

---
+++ src/pages/Events.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import PageHeader from "../components/PageHeader";
+
+export default function Events() {
+    return (
+        <>
+            <PageHeader height="small">
+                <Typography variant="h2">Activități</Typography>
+            </PageHeader>
+        </>
+    );
+}
