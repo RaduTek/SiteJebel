a9462a1034589409133988adc0473f2b309587cb
RaduTek <radutek13@gmail.com>
2024-05-15 22:56:54 +0300

    Add banner image to landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,18 @@
 import { Typography } from "@mui/material";
+import PageSection from "../components/PageSection";
+import PageHeader from "../components/PageHeader";
+import LandingImage from "../images/banners/landing.jpg";
+import Logo from "../components/Logo";
 
 export default function Landing() {
-    return <Typography>Landing page</Typography>;
+    return (
+        <>
+            <PageHeader height="large" image={LandingImage} verticalAlign="end">
+                <Logo variant="banner" />
+            </PageHeader>
+            <PageSection>
+                <Typography>Landing page</Typography>
+            </PageSection>
+        </>
+    );
 }
