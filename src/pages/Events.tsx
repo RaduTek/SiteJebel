e688389bd754f51fe85ff0b2bd28324336a3b637
RaduTek <radutek13@gmail.com>
2024-05-15 22:56:42 +0300

    Add timeline to events page

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,12 +1,39 @@
-import { Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import PageHeader from "../components/PageHeader";
+import PageSection from "../components/PageSection";
+import Timeline from "../components/Timeline";
+import { useNavigate } from "react-router-dom";
+import handleRouterPush from "../utils/handleRouterPush";
 
-export default function Events() {
+export default function Events({ past }: { past?: boolean }) {
+    const navigate = useNavigate();
+
     return (
         <>
             <PageHeader height="small">
                 <Typography variant="h2">Activități</Typography>
+                <Stack direction="row" gap={2}>
+                    <Button
+                        component="a"
+                        href="/events"
+                        onClick={handleRouterPush(navigate)}
+                        variant={past ? "outlined" : "contained"}
+                    >
+                        Următoare
+                    </Button>
+                    <Button
+                        component="a"
+                        href="/events/past"
+                        onClick={handleRouterPush(navigate)}
+                        variant={past ? "contained" : "outlined"}
+                    >
+                        Din Trecut
+                    </Button>
+                </Stack>
             </PageHeader>
+            <PageSection sx={{ paddingBottom: 0 }}>
+                <Timeline past={past} />
+            </PageSection>
         </>
     );
 }
