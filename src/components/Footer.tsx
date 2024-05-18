61755151dd5fb5f232a8ec224007e3555c3ce9dd
RaduTek <radutek13@gmail.com>
2024-05-18 16:59:27 +0300

    Add logo to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,13 @@
 import { Box } from "@mui/material";
+import PageSection from "./PageSection";
+import Logo from "./Logo";
 
 export default function Footer() {
-    return <Box></Box>;
+    return (
+        <Box sx={{ height: "20vh", backgroundColor: "#eee" }}>
+            <PageSection sx={{ alignItems: "center" }}>
+                <Logo variant="footer" />
+            </PageSection>
+        </Box>
+    );
 }
