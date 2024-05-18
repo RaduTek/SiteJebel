a5052705fa695459646149f7a273ea1558b94830
RaduTek <radutek13@gmail.com>
2024-05-18 13:58:16 +0300

    Move landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,8 +1,8 @@
 import { Typography } from "@mui/material";
-import PageSection from "../components/PageSection";
-import PageHeader from "../components/PageHeader";
-import LandingImage from "../images/banners/landing.jpg";
-import Logo from "../components/Logo";
+import PageSection from "../../components/PageSection";
+import PageHeader from "../../components/PageHeader";
+import LandingImage from "./banner.jpg";
+import Logo from "../../components/Logo";
 
 export default function LandingPage() {
     return (
