45581f313a12b0dc0ac0e1f6cfe0a2052f5086b1
RaduTek <radutek13@gmail.com>
2024-05-21 19:51:22 +0300

    Implement loading and error handling

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -2,20 +2,43 @@ import { useEffect, useState } from "react";
 import "./Timeline.css";
 import TimelineEvent from "./TimelineEvent";
 import Event from "./Event";
-import { Stack, Typography } from "@mui/material";
+import { CircularProgress, Stack, Typography } from "@mui/material";
 
 export default function Timeline({ past }: { past?: boolean }) {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
     const [events, setEvents] = useState<Event[]>([]);
 
     useEffect(() => {
+        setLoading(true);
+        setError(false);
         fetch("/api/public/events.php" + (past ? "?past" : ""))
             .then((res) => res.json())
             .then((data) => {
                 setEvents(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.log(error);
+                setError(true);
             });
     }, [past, setEvents]);
 
-    return events.length > 0 ? (
+    return error ? (
+        <Stack sx={{ paddingTop: 3, textAlign: "center", gap: 2 }}>
+            <Typography variant="h4">Eroare</Typography>
+            <Typography>
+                A avut loc o eroare la încărcarea activităților.
+            </Typography>
+        </Stack>
+    ) : loading ? (
+        <Stack sx={{ paddingTop: 3, textAlign: "center", gap: 2 }}>
+            <Typography variant="h4">Încărcare date</Typography>
+            <Stack alignItems="center">
+                <CircularProgress color="inherit" size={80} />
+            </Stack>
+        </Stack>
+    ) : events.length > 0 ? (
         <div className="timelineRoot">
             {events.map((data, index) => (
                 <TimelineEvent key={index} data={data} />
