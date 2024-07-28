6da0143ca4c237ee334c986eee6d74852e1267e0
RaduTek <me@radutek.uk>
2024-07-28 19:46:38 +0300

    New authentication files

--- api/auth.php
+++ api/auth.php
@@ -1,25 +1,110 @@
 <?php
 
-function start_auth() {
+/**
+ * Sets the user data in the session.
+ *
+ * @param array $user An associative array containing user data.
+ */
+function set_user(array $user)
+{
+    $_SESSION['user'] = $user;
+}
+
+/**
+ * Unsets the user data from the session.
+ */
+function unset_user()
+{
+    unset($_SESSION['user']);
+}
+
+/**
+ * Retrieves the user data from the session if the user is authenticated.
+ *
+ * @return array|false An associative array containing user data, or false if the user is not authenticated.
+ */
+function get_user(): array|false
+{
+    if (is_authed())
+        return $_SESSION['user'];
+    return false;
+}
+
+/**
+ * Starts the session for authentication purposes.
+ */
+function start_auth()
+{
     session_start();
 }
 
-function set_session($data) {
-    foreach (array_keys($data) as $key) {
-        $_SESSION[$key] = $data[$key];
+/**
+ * Ends the session by unsetting user data and destroying the session.
+ */
+function end_auth()
+{
+    unset_user();
+    session_destroy();
+}
+
+/**
+ * Checks if the user is authenticated.
+ *
+ * @return bool True if the user is authenticated, false otherwise.
+ */
+function is_authed(): bool
+{
+    return isset($_SESSION['user']);
+}
+
+/**
+ * Checks if the authenticated user has administrative privileges.
+ *
+ * @return bool True if the authenticated user is an admin, false otherwise.
+ */
+function is_admin(): bool
+{
+    if (is_authed())
+        return get_user()['isAdmin'];
+    return false;
+}
+
+/**
+ * Collects authentication data, excluding the password, for the authenticated user.
+ *
+ * @return array An associative array containing authentication data and user information.
+ */
+function get_auth_data(): array
+{
+    $auth_data = [];
+    $auth_data['isAuthed'] = is_authed();
+
+    if (is_authed()) {
+        $user_data = strip_keys(get_user(), User_sensitive_keys);
+        $auth_data += $user_data;
     }
+
+    return $auth_data;
 }
 
-function is_authed() {
-    return isset($_SESSION['userId']);
-}
-
-function exit_if_unauthed() {
+/**
+ * Sends a JSON response and exits if the user is not authenticated.
+ */
+function exit_if_unauthed()
+{
     if (!is_authed()) {
-        return_json(["message" => "Unauthorized to perform this action."]);
+        return_json(["error" => "Unauthorized to perform this action."]);
         exit(401);
     }
 }
 
-
-?>
+/**
+ * Sends a JSON response and exits if the authenticated user is not an admin.
+ */
+function exit_if_not_admin()
+{
+    if (!is_admin()) {
+        return_json(["error" => "Unauthorized to perform this action."]);
+        exit(401);
+    }
+}
