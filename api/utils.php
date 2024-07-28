1a70482561874fbcdee58a2268a1031009561469
RaduTek <me@radutek.uk>
2024-07-28 19:28:23 +0300

    Add descriptive comments

--- api/utils.php
+++ api/utils.php
@@ -1,17 +1,13 @@
 <?php
 
-function include_page($page_name, $root_path) {
-    $page_file = $root_path . $page_name;
-    if (file_exists($page_file . '.html')) {
-        include_once($page_file . '.html');
-    } else if (file_exists($page_file . '.php')) {
-        include_once($page_file . '.php');
-    } else {
-        echo 'Page not found.';
-    }
-}
-
-function new_guid($data = null) {
+/**
+ * Generates a new GUID (Globally Unique Identifier).
+ *
+ * @param string|null $data Optional binary data to generate the GUID.
+ * @return string The generated GUID.
+ */
+function new_guid($data = null)
+{
     $data = $data ?? random_bytes(16);
     assert(strlen($data) == 16);
 
@@ -21,21 +17,50 @@ function new_guid($data = null) {
     return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 }
 
-function redirect($new_url, $code = 301) {
-    header("Location: " . $new_url, true, $code);
+/**
+ * Redirects the browser to a new URL with the specified status code.
+ *
+ * @param string $new_url The URL to redirect to.
+ * @param int $status_code The HTTP status code for the redirection. Default is 301.
+ */
+function redirect(string $new_url, int $status_code = 301)
+{
+    header("Location: " . $new_url, true, $status_code);
 }
 
-function get_json() {
+/**
+ * Retrieves JSON data from the HTTP request body and decodes it into an associative array.
+ *
+ * @return array The decoded JSON data.
+ */
+function get_json(): array
+{
     $json_data = file_get_contents('php://input');
-    return json_decode($json_data, true);
+    return json_decode($json_data, associative: true);
 }
 
-function return_json($data) {
+/**
+ * Sends a JSON response with the specified data and status code.
+ *
+ * @param object $data The data to be encoded as JSON and returned.
+ * @param int $status_code The HTTP status code for the response. Default is 200.
+ */
+function return_json(object $data, int $status_code = 200)
+{
     header('Content-Type: application/json');
-    echo(json_encode($data));
+    http_response_code($status_code);
+    echo (json_encode($data));
 }
 
-function check_set_keys($keys, $array) {
+/**
+ * Checks if an associative array contains specified keys.
+ *
+ * @param array $keys The keys to check for in the array.
+ * @param array $array The array to check.
+ * @return bool True if all specified keys are present, false otherwise.
+ */
+function check_set_keys(array $keys, array $array)
+{
     foreach ($keys as $key) {
         if (!isset($array[$key]))
             return false;
