6a0b7e6133525f4300c4951a3629718dbbe0bfdf
RaduTek <me@radutek.uk>
2024-07-28 19:17:15 +0300

    New environment file layout

--- api/env.sample.php
+++ api/env.sample.php
@@ -1,10 +1,31 @@
 <?php
 
-$ENV['DEBUG'] = false;
+// Database settings ============================
 
+// Database type (mysql, pgsql or sqlite)
+$ENV['DB_TYPE'] = 'mysql';
+
+// Database host name or IP address (for mysql and pgsql)
 $ENV['DB_HOST'] = '';
-$ENV['DB_USER'] = '';
-$ENV['DB_PASS'] = '';
+
+// Database name (for mysql and pgsql)
 $ENV['DB_NAME'] = '';
 
-?>
+// Connection user name (for mysql and pgsql)
+$ENV['DB_USER'] = '';
+
+// Connection password (for mysql and pgsql)
+$ENV['DB_PASS'] = '';
+
+// SQlite database file (for sqlite only)
+$ENV['DB_FILE'] = '';
+
+// Database generate script credentials (/api/database_generate.php)
+// Used only to limit access to the script
+$ENV['DBGEN_USER'] = 'change-me';
+$ENV['DBGEN_PASS'] = 'change-me';
+
+// Other settings ===============================
+
+// Enable or disable debug
+$ENV['DEBUG'] = false;
