1d7b675fb6d8f14fbab57e6a840fd3675a278881
RaduTek <me@radutek.uk>
2024-07-29 00:42:15 +0300

    Update public events endpoint

--- api/public/events.php
+++ api/public/events.php
@@ -1,49 +1,23 @@
 <?php
 
-class Event {
-    public int $id;
-    public string $date;
-    public string $title;
-    public string $photoId;
-    public string $color;
-    public string $description;
-    public array $links;
-}
-
 require_once('../include.php');
 $pdo = db_connect();
 
-$query = isset($_GET['past']) 
-    ? "SELECT * FROM events WHERE date < NOW() AND visible = 1 ORDER BY date DESC"
-    : "SELECT * FROM events WHERE date >= NOW() AND visible = 1 ORDER BY date ASC";
+try {
+    $crud = new CRUD($pdo, Table_Events);
 
-$stmt = $pdo -> query($query);
-$events = [];
+    $filters = ['visible' => 1];
+    $order_by = 'date';
+    $order_direction = isset($_GET['past']) ? 'DESC' : 'ASC';
+    $filters['date'] = isset($_GET['past']) ? ['<', 'NOW()'] : ['>=', 'NOW()'];
 
-while ($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
-    $event = new Event();
-    $event->id = (int)$row['id'];
-    $event->date = (new DateTime($row['date']))->format(DateTime::ATOM);
-    $event->title = (string)$row['title'];
-    $event->photoId = (string)$row['photoId'];
-    $event->color = (string)$row['color'];
-    $event->description = (string)$row['description'];
-    
-    $links = json_decode($row['links'], true);
-    if (isset($_GET['past'])) {
-        $links = array_filter($links, function ($link) {
-            return $link['visibleAfterEvent'];
-        });
-        $links = array_values($links);
-    }
-    foreach ($links as &$link) {
-        unset($link['visibleAfterEvent']);
-    }
-    
-    $event->links = $links;
-    $events[] = $event;
+    $events = $crud->list(
+        filters: $filters,
+        order_by: $order_by,
+        order_direction: $order_direction
+    );
+
+    return_json($events);
+} catch (Exception $e) {
+    return_json(['error' => $e->getMessage()], 500);
 }
-
-return_json($events);
-
-?>
