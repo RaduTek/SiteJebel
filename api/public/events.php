0dcba88378f605b71c023c91814d953c86ad6ca4
RaduTek <me@radutek.uk>
2024-08-02 14:21:53 +0300

    Use now query

--- api/public/events.php
+++ api/public/events.php
@@ -4,12 +4,16 @@ require_once('../include.php');
 $pdo = db_connect();
 
 try {
-    $crud = new CRUD($pdo, Table_Events);
+    $crud = new CRUD($pdo, Events);
 
     $filters = ['visible' => 1];
     $order_by = 'date';
     $order_direction = isset($_GET['past']) ? 'DESC' : 'ASC';
-    $filters['date'] = isset($_GET['past']) ? ['<', 'NOW()'] : ['>=', 'NOW()'];
+
+    $currentDate = date('Y-m-d H:i:s');
+    $filters['date'] =
+        isset($_GET['past']) ? ['operator' => '<', 'value' => $currentDate]
+        : ['operator' => '>=', 'value' => $currentDate];
 
     $events = $crud->list(
         filters: $filters,
