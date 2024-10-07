31cc6eab40180f2b3702587cf60cd3b8cac15402
RaduTek <me@radutek.uk>
2024-10-07 11:01:19 +0300

    Add continue data for course

--- backend/api/public/course/details.php
+++ backend/api/public/course/details.php
@@ -2,6 +2,7 @@
 
 require_once('../../include.php');
 $pdo = db_connect();
+start_auth();
 
 if (!isset($_GET['id'])) {
     return_json(["status" => "not_found"], 404);
@@ -10,16 +11,35 @@ if (!isset($_GET['id'])) {
 $id = $_GET['id'];
 
 try {
-    $crud = new CRUD($pdo, Courses);
+    $crud1 = new CRUD($pdo, Courses);
+    $crud2 = new CRUD($pdo, Course_Progress);
 
     $columns = ['id', 'title', 'difficulty', 'shortdesc', 'description'];
 
-    $courses = $crud->read(
+
+    $course = $crud1->read(
         id: $id,
         columns: $columns
     );
 
-    return_json($courses);
+    $result = $course;
+
+    if (is_authed()) {
+        $user = get_user();
+
+        $filters = ['user_id' => $user['id']];
+
+        $progress = $crud2->list($filters, order_by: 'start_date', order_direction: 'DESC', limit: 1);
+
+        if (count($progress) > 0) {
+            $progress = $progress[0];
+            $result['continuePageId'] = $progress['content_progress'];
+        } else {
+            $result['continuePageId'] = 0;
+        }
+    }
+
+    return_json($result);
 } catch (Exception $e) {
     return_json(['error' => $e->getMessage()], 500);
 }
